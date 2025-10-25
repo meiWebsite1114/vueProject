e7b547df5c63fe0c7c1ade9808dcf36185c98863
meiWebsite1114 <miji11142@gmail.com>
2025-10-25 09:42:44 +0800

    調整

--- V002/src/router/index.js
+++ V002/src/router/index.js
@@ -1,10 +1,14 @@
-import { createRouter, createWebHistory } from "vue-router";
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory,
+} from "vue-router";
 import PostListView from "../views/PostListView.vue";
 import PostDetailView from "../views/PostDetailView.vue";
 import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: "/",
